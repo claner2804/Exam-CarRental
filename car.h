0bc75953530c4288850c4a46aeae770df956ac9f
claner2804 <claner2804@gmail.com>
2024-05-18 13:37:36 +0200

    .

--- car.h
+++ car.h
@@ -9,6 +9,21 @@
 
 class Car : public CarRental{ //todo: check if inheritance is correct
 
+public:
+    //Überprüft die Fahrtüchtigkeit des Autos
+    // und gibt true zurück, wenn die Fahrtüchtigkeit gegeben ist.
+    // Bei gefundenen Problemen wird eine Exception geworfen
+    virtual bool checkCar() = 0;
+
+
+    //Gibt die benötigte Führerscheinklasse zurück.
+    virtual int getRequiredDrivingLicense() = 0;
+
+
+    //Gibt die Anzahl der Passagierplätze zurück.
+    virtual int getPassengerCount() = 0;
+
+
 };
 
 
--- vwBus.h
+++ vwBus.h
@@ -6,8 +6,60 @@
 #define KLAUSUR_VWBUS_H
 
 #include "car.h"
+#include <iostream>
+#include <cstdlib>
+#include "exceptions.h"
 
-class VwBus : public Car
+//		○ 8 Passagierplätze
+//		○ Führerscheinklasse: 2
+//		○ Hat eine 5% Wahrscheinlichkeit eine BrokenMotorException zu werfen.
+//		○ Hat eine 10% Wahrscheinlichkeit eine ElectronicsFaultException zu werfen.
+//Eine EmissionsTooDirtyException kann bei diesem Fahrzeug nicht vorkommen.
+
+class VwBus : public Car{
+
+    int passengerSeats = 8;
+
+    int requiredDriversLicense = 2;
+
+public:
+
+    int getRequiredDrivingLicense() override {
+        return requiredDriversLicense;
+    }
+
+    int getPassengerCount() override {
+        return passengerSeats;
+    }
+
+    //Hat eine 5% Wahrscheinlichkeit eine BrokenMotorException zu werfen.
+    bool isBrokenMotor()  {
+        if (rand() % 100 < 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Hat eine 10% Wahrscheinlichkeit eine ElectronicsFaultException zu werfen.
+    bool isElectronicsFault()  {
+        if (rand() % 100 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool checkCar() override {
+        if (isBrokenMotor()) {
+            throw BrokenMotorException();
+        } else if (isElectronicsFault()) {
+            throw ElectronicsFaultException();
+        } else {
+            return true;
+        }
+    }
+
 
 };
 
